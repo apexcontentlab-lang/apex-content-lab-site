1e4696500691afdf1581a5ee7e5f866c09d2f1d4
Apex Content Lab <apexcontentlab@gmail.com>
2026-08-10 19:17:06 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://apex-content-lab.com',
   integrations: [
     tailwind(),
-    sitemap({
-      filter: (page) => !page.includes('/404'),
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-    }),
+    sitemap()
   ],
 });
+
